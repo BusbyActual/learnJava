3798d55c9489a40b4d106613beccb05216d4f0df
Ryan Busby <BusbyActual@gmail.com>
2018-02-03 12:41:50 -0800

    Added solution for prompt 1

---
+++ src/1. Up and going/Solutions/1. HelloWorld.java
@@ -0,0 +1,10 @@
+// Update the code to print Hello World to the console.
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // System.out.println prints to the terminal window.
+        System.out.println("Hello World");
+    }
+
+}
